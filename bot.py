f96204302bac01e0bef3b3ad8465445c3c239638
Promobot1 <162498690+Promobot1@users.noreply.github.com>
2024-03-09 10:52:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, User
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 TOKEN = '7027145577:AAG0KlWZqSDQjPZPOwCgHpJDHht5aJzZxNc'
@@ -11,16 +11,23 @@ def delete_links(update: Update, context: CallbackContext) -> None:
     message = update.message
     user = message.from_user
     if user and user.username:
-        bio = context.bot.get_user_profile_photos(user.id).photos[0][0].get_file().file_path
-        if "t.me" in bio:
+        bio = get_user_bio(user, context)
+        if bio and "t.me" in bio:
             message.delete()
+
+def get_user_bio(user: User, context: CallbackContext) -> str:
+    try:
+        bio = context.bot.get_user_profile_photos(user.id).photos[0][0].get_file().file_path
+        return bio
+    except IndexError:
+        return ""
 
 def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.bio, delete_links))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, delete_links))
 
     updater.start_polling()
     updater.idle()
